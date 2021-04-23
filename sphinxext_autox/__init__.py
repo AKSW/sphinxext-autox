3ad2e9c74d3a3e6dcdf63f87f7902e8af02180af
Simon Bin <sbin@informatik.uni-leipzig.de>
2021-04-24 01:27:18 +0200

    fix paths

--- sphinxext_autox/__init__.py
+++ sphinxext_autox/__init__.py
@@ -7,8 +7,12 @@ __version__ = '0.0.1'
 sphinx.ext.__path__.insert(
     0,
     os.path.abspath(
-        os.sep.join([os.path.dirname(__file__), '..', 'sphinx', 'ext'])))
-print("SPHINX EXT PATH IS NOW", sphinx.ext.__path__)
+        os.sep.join([os.path.dirname(__file__), 'sphinx', 'ext'])))
+print("[sphinxext_autox] replacing built-in exts, sphinx.ext.__path__ is now", sphinx.ext.__path__)
 
 def setup(app):
-    return dict()
+    app.setup_extension('sphinx.ext.autosummary')
+    app.setup_extension('sphinx.ext.autodoc')
+    return {
+        'version': __version__,
+    }
